a2584da7a1a5b8aa8ca34ce0d35cfd651dfb40a2
Jonathan Sick <jonathan@jsick.codes>
2023-03-13 13:04:24 -0300

    Set up testing again project rools
    The sphinx.testing module lets us create test documentation projects to
    validate that extensions render as expected. Test projects are located
    in tests/roots.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,21 @@
 """Pytest configuration and fixtures."""
+
+import pytest
+from sphinx.testing.path import path
+
+pytest_plugins = ("sphinx.testing.fixtures",)
+
+# Exclude 'roots' dirs for pytest test collector
+collect_ignore = ["roots"]
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    config.addinivalue_line(
+        "markers", "sphinx(builder, testroot='name'): Run sphinx on a site"
+    )
+
+
+@pytest.fixture(scope="session")
+def rootdir() -> path:
+    """Directory containing Sphinx projects for testing (`str`)."""
+    return path(__file__).parent.abspath() / "roots"
