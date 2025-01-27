2c25c100bd1916a8830621183d7abd394031b953
Jonathan Sick <jonathan@jsick.codes>
2025-01-27 13:00:07 -0500

    Support bibtex bibliographies in technotes
    - Add sphinxcontrib.bibtex to provide support for cite roles and
      bibliography directives in technotes.
    - Add Rubin's Documenteer package as an extension to provide the
      githubbibcache extension that can download a bibtex file from GitHub
    - Configure SPHEREx technotes to always use the spherex.bib file from
      GitHub, and also load any .bib bibliographies from the local
      repository.

--- demo/md-technote/index.md
+++ demo/md-technote/index.md
@@ -6,4 +6,10 @@ The abstract is a brief summary of the document. In technotes it is specially ma
 
 ## Introduction
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nunc maximus ultricies. Nullam nec magna nec nuncegestas tincidunt ut ac justo.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nunc maximus ultricies. Nullam nec magna nec nuncegestas tincidunt ut ac justo. {cite}`SPHEREx_SPIE`
+
+## References
+
+```{bibliography}
+
+```
--- src/spherexsphinx/conf/technote.py
+++ src/spherexsphinx/conf/technote.py
@@ -1,14 +1,22 @@
 """Sphinx configuration for SPHEREx technotes."""
 
+from contextlib import suppress
 from pathlib import Path
 
 from technote.sphinxconf import *  # noqa: F401 F403
+
+with suppress(ValueError):
+    # Remove the sphinxcontrib-bibtex extension so that we can add it back
+    # in the proper order relative to documenteer.ext.githubbibcache.
+    extensions.remove("sphinxcontrib.bibtex")  # noqa: F405
 
 extensions.extend(  # noqa: F405
     [
         "sphinxcontrib.mermaid",
         "sphinx_prompt",
         "sphinx_design",
+        "documenteer.ext.githubbibcache",
+        "sphinxcontrib.bibtex",
     ]
 )
 
@@ -26,3 +34,20 @@ html_theme_options = {
     "logo_link_url": "https://spherex-docs.ipac.caltech.edu",
     "logo_alt_text": "SPHEREx",
 }
+
+# Configure bibliography with the bib cache
+documenteer_bibfile_cache_dir = ".technote/bibfiles"
+documenteer_bibfile_github_repos = [
+    {
+        "repo": "SPHEREx/spherex-tex",
+        "ref": "main",
+        "bibfiles": [
+            "texmf/bibtex/bib/spherex.bib",
+        ],
+    }
+]
+# Set up bibtex_bibfiles
+# Automatically load local bibfiles in the root directory.
+bibtex_bibfiles = [str(p) for p in Path.cwd().glob("*.bib")]
+
+bibtex_reference_style = "author_year"
