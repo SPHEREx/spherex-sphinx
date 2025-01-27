7f1ef32687507d35b3805f05779cfe71a57f5392
Jonathan Sick <jonathan@jsick.codes>
2025-01-27 12:09:03 -0500

    Create spherexsphinx.conf.technote
    This preset configuration provides support for SPHEREx technotes using
    the technote framework (https://technote.lsst.io). The additionl
    dependencies for this documentation mode are included through the
    [technote] dependency extra to avoid adding unnecessary dependencies to
    software projects using spherexsphinx for user guides.
    
    The demo/md-technote/ sample project illustrates the features of SPHEREx
    Sphinx technotes. It can be built by running `tox run -e demo`.

---
+++ demo/md-technote/index.md
@@ -0,0 +1,9 @@
+# Test SPHEREx Technote
+
+```{abstract}
+The abstract is a brief summary of the document. In technotes it is specially marked up with an `abstract` directive.
+```
+
+## Introduction
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nunc maximus ultricies. Nullam nec magna nec nuncegestas tincidunt ut ac justo.
---
+++ src/spherexsphinx/conf/technote.py
@@ -0,0 +1,20 @@
+"""Sphinx configuration for SPHEREx technotes."""
+
+from pathlib import Path
+
+from technote.sphinxconf import *  # noqa: F401 F403
+
+_assets_dir = Path(__file__).parent.joinpath("../assets")
+
+html_static_path: list[str] = [
+    str(_assets_dir.joinpath("spherex-logo-color-dark.png").resolve()),
+    str(_assets_dir.joinpath("spherex-logo-color-light.png").resolve()),
+]
+
+# Configurations for the technote theme.
+html_theme_options = {
+    "light_logo": "spherex-logo-color-light.png",
+    "dark_logo": "spherex-logo-color-dark.png",
+    "logo_link_url": "https://spherex-docs.ipac.caltech.edu",
+    "logo_alt_text": "SPHEREx",
+}
